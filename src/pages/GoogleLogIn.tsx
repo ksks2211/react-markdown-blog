91d7e1b079deaeb8354788b824eea9f023783bae
ksks2211 <rival15@naver.com>
2024-03-10 08:13:59 +0900

    update

--- src/pages/GoogleLogIn.tsx
+++ src/pages/GoogleLogIn.tsx
@@ -6,14 +6,10 @@ function GoogleLogIn() {
   const [oauth2Params] = useSearchParams();
   const mutation = useLoginWithOAuth2();
 
-  oauth2Params.forEach((value, key) => {
-    console.log(`V:${value}, K : ${key}`);
-  });
-
   mutation.mutate(oauth2Params);
 
   if (mutation.isError) {
-    const message = encodeURIComponent(mutation.error.message);
+    const message = encodeURIComponent(mutation.error.message || "Login Fail");
     return <Navigate to={`/login?error=${message}`} />;
   }
 
